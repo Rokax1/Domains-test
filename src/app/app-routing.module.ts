6a54667973a46fac759847cdb9a000bdd0a02d48
leandro <leandro@nublelabs.cl>
2024-07-30 16:07:38 -0400

    feat: apply guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,20 @@ import { AcercaComponent } from './acerca/acerca.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './dashboard/profile/profile.component';
 import { SettingsComponent } from './dashboard/settings/settings.component';
+import { DomainGuard } from './guards/domain.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  { path: 'inicio', component: InicioComponent },
-  { path: 'acerca', component: AcercaComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' ,canActivate: [DomainGuard]},
+  { path: 'inicio', component: InicioComponent, canActivate: [DomainGuard] },
+  { path: 'acerca', component: AcercaComponent, canActivate: [DomainGuard] },
   {
     path: 'dashboard',
     component: DashboardComponent,
     children: [
       { path: 'profile', component: ProfileComponent },
       { path: 'settings', component: SettingsComponent }
-    ]
+    ],
+    canActivate: [DomainGuard]
   },
   { path: '**', redirectTo: '/inicio' } // redirige cualquier ruta no encontrada al inicio
 ];
